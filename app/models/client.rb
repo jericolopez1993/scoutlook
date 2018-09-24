779b14a0267aaf18eff2e8c3e7c280393cf9eb1c
Kevin Marcelo <kevin@marcelo.ph>
2018-09-24 22:47:14 +0800

    Self Generate Client ID

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,29 @@
-class Client < ApplicationRecord
+# t.string "client_type"
+# t.integer "relationship_owner"
+# t.string "company_name"
+# t.string "client_id"
+# t.string "parent_id"
+# t.integer "sales_priority"
+# t.string "address"
+# t.string "city"
+# t.string "state"
+# t.string "postal"
+# t.string "country"
+# t.string "phone"
+# t.decimal "annual_revenue"
+# t.string "industry"
+# t.string "primary_industry"
+# t.string "hazardous"
+# t.string "food_grade"
+# t.decimal "freight_revenue"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
 
-  SHIPPER = 'Shipper'
-  CARRIER = 'Carrier'
-  ROLES = [SHIPPER, CARRIER]
+class Client < ApplicationRecord
+  before_create :set_client_id
+
+  def set_client_id
+    count = Client.all.length.to_s.rjust(5, '0')
+    self.client_id = "LOOP" + count
+  end
 end
