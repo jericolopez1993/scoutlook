f41fd11f0ea07a47e405f55f5e66940c9d79ff65
Kevin Marcelo <kevin@marcelo.ph>
2018-09-26 02:28:37 +0800

    Minor Fixed on the Views

--- app/models/client.rb
+++ app/models/client.rb
@@ -20,10 +20,10 @@
 # t.datetime "updated_at", null: false
 
 class Client < ApplicationRecord
-  before_create :set_client_id
-
-  def set_client_id
-    count = (Client.all.length + 1).to_s.rjust(5, '0')
-    self.client_id = "LOOP" + count
-  end
+  # before_create :set_client_id
+  #
+  # def set_client_id
+  #   count = (Client.all.length + 1).to_s.rjust(5, '0')
+  #   self.client_id = "LOOP" + count
+  # end
 end
--- app/models/client_location.rb
+++ app/models/client_location.rb
@@ -1,8 +1,8 @@
 class ClientLocation < ApplicationRecord
-  before_create :set_location_id
-
-  def set_location_id
-    count = (ClientLocation.all.length + 1).to_s.rjust(5, '0')
-    self.location_id = "ENG" + count
-  end
+  # before_create :set_location_id
+  #
+  # def set_location_id
+  #   count = (ClientLocation.all.length + 1).to_s.rjust(5, '0')
+  #   self.location_id = "ENG" + count
+  # end
 end
