5ceacb3420358652ac0443af9bc20b1622b96fdd
Kevin Marcelo <kevin@marcelo.ph>
2018-10-11 02:12:02 +0800

    Change Annual Value's Options

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Activity.all.each do |activity|
+  if activity.annual_value == '0 - 5,000'
+    activity.update_attributes(:annual_value => '0-5M')
+  elsif activity.annual_value == '5,000 - 25,000'
+    activity.update_attributes(:annual_value => '5-10M')
+  elsif activity.annual_value == '25,000 - 50,000'
+    activity.update_attributes(:annual_value => '10-25M')
+  elsif activity.annual_value == '50,000 - 100,000'
+    activity.update_attributes(:annual_value => '25-100M')
+  elsif activity.annual_value == '100,000+'
+    activity.update_attributes(:annual_value => '100M+')
+  end
+end
