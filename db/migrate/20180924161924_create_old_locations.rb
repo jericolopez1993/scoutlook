b73c1f54bbfb791878c82974fdf521541aae579a
Kevin Marcelo <kevin@marcelo.ph>
2018-10-22 18:34:25 +0800

    Fix Migrations and Remove Console Logs on Address Form

---
+++ db/migrate/20180924161924_create_old_locations.rb
@@ -0,0 +1,19 @@
+class CreateOldLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :client_id
+      t.string :location_id
+      t.string :name
+      t.string :loc_type
+      t.text :special_instructions
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :postal
+      t.string :country
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
