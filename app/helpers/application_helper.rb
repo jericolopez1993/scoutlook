f083cde93d88717e9b68624fd596ec18638295a8
Kevin Marcelo <kevin@marcelo.ph>
2018-10-23 02:33:57 +0800

    Change Generated IDs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,25 @@
 module ApplicationHelper
 
-  def generate_alphanumeric_id(str, id)
-    count = id.to_s.rjust(5, '0')
+  def generate_alphanumeric_id(str = "", id)
+    count = id.to_s.rjust(3, '0')
     if str.nil?
-      str = "NONM"
+      str = ""
     end
-    alp = str[0, 4].upcase
+    alp = str.gsub(/[^0-9A-Za-z]/, '')[0, 3].upcase
     alp + count
+  end
+
+  def generate_two_alphanumeric_id(str1 = "", str2 = "", id)
+    count = id.to_s.rjust(3, '0')
+    if str1.nil?
+      str1 = ""
+    end
+    if str2.nil?
+      str2 = ""
+    end
+    alp1 = str1.gsub(/[^0-9A-Za-z]/, '')[0, 3].upcase
+    alp2 = str2.gsub(/[^0-9A-Za-z]/, '')[0, 3].upcase
+    alp1 + alp2  + count
   end
 
   def convert_date(val)
