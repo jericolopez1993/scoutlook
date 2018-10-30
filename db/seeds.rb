e9aa8353f84b0e18c48495b0351503fdea15eed3
Kevin Marcelo <kevin@marcelo.ph>
2018-10-31 01:00:35 +0800

    Minor Fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-Client.find(3).update_attributes(:head_office => 21)
-
-Location.all.each do |location|
-  location.update_attributes(:client_id => 3)
+# Client.find(3).update_attributes(:head_office => 21)
+#
+# Location.all.each do |location|
+#   location.update_attributes(:client_id => 3)
+# end
+#
+# Location.find(21).update_attributes(:is_origin => true, :is_destination => true
+# User.create!([
+#   {id: 6, email: "test@marcelo.ph", password: "password", password_confirmation: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, created_at: "2018-10-14 14:53:58", updated_at: "2018-10-27 20:33:03", first_name: "Test", middle_name: nil, last_name: "Contact", confirmation_token: nil, confirmed_at: "2018-10-14 14:53:58", confirmation_sent_at: nil, admin: false, client_contact_id: 12}
+# ])
+MasterInvoice.all.each do |invoice|
+  if invoice.shipment_entry == "single shipment"
+    invoice.update_attributes(:shipment_entry => 'Single')
+  elsif invoice.shipment_entry == "multiple shipment"
+    invoice.update_attributes(:shipment_entry => 'Multiple')
+  end
 end
-
-Location.find(21).update_attributes(:is_origin => true, :is_destination => true)
