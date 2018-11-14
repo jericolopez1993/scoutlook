95dd69d6aca8b61be7c7c5065fee0879347470b3
Kevin Marcelo <kevin@marcelo.ph>
2018-11-14 23:17:43 +0800

    Audit Fix

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -13,10 +13,16 @@ class Audit
     rescue
       @to_sentence = "Unknown"
     end
-
-    class_object = self.auditable_type.singularize.classify.constantize.find(self.auditable_id)
-    @to_sentence = @to_sentence + " " + self.action + " an #{class_object.display_name}"
-
+    if self.action == "destroy"
+      @to_sentence = @to_sentence + " removed an #{self.auditable_type == 'MasterInvoice' ? 'Invoices' : (self.auditable_type == 'MasterSignal' ? 'Signals' : self.auditable_type.titleize)}"
+    else
+      begin
+        class_object = self.auditable_type.singularize.classify.constantize.find(self.auditable_id)
+        @to_sentence = @to_sentence + " " + self.action + " an #{class_object.display_name}"
+      rescue
+        @to_sentence = @to_sentence + " " + self.action + " an #{self.auditable_type == 'MasterInvoice' ? 'Invoices' : (self.auditable_type == 'MasterSignal' ? 'Signals' : self.auditable_type.titleize)}"
+      end
+    end
     @to_sentence
   end
 end
