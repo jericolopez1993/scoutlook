c240430cf14da52df1391aa274e9ac97b3e1d5e7
Kevin Marcelo <kevin@marcelo.ph>
2018-12-06 11:23:50 +0800

    SC-13: Additional Changes for ADM

--- db/migrate/20181206002134_add_alternative_decision_maker_to_carrier_and_shipper.rb
+++ db/migrate/20181206002134_add_alternative_decision_maker_to_carrier_and_shipper.rb
@@ -1,6 +1,11 @@
 class AddAlternativeDecisionMakerToCarrierAndShipper < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :carrier_contacts, :adm, :boolean, :default => false
     add_column :shipper_contacts, :adm, :boolean, :default => false
+  end
+
+  def down
+    remove_column :carrier_contacts, :adm
+    remove_column :shipper_contacts, :adm
   end
 end
