74256260270a9f50237229332beac6c235fdb373
Kevin Marcelo <kevin@marcelo.ph>
2019-01-12 11:03:53 +0800

    SC-59: Add the Rate table on Edit Activity Page

---
+++ app/controllers/api/activities_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  class ActivitiesController < ApplicationController
+    def index
+      if params[:carrier_id].present?
+        @activities = Activity.where(:carrier_id => params[:carrier_id])
+      elsif params[:shipper_id].present?
+        @activities = Activity.where(:shipper_id => params[:shipper_id])
+      else
+        @activities = []
+      end
+      render json: @activities, methods: [:full_name]
+    end
+  end
+end
