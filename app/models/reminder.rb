99c42a55052bb168428771bd1d0f552656ca618f
Kevin Marcelo <kevin@marcelo.ph>
2019-01-19 03:35:00 +0800

    SC-62: Reminder Index View Page

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,2 +1,6 @@
 class Reminder < ApplicationRecord
+  belongs_to :carrier, optional: true
+  belongs_to :shipper, optional: true
+  belongs_to :activity, optional: true
+  belongs_to :user, optional: true
 end
