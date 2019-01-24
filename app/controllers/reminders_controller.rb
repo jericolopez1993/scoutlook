0acc1b51d939c5161bc1a185aef42f22e2e44cb0
Kevin Marcelo <kevin@marcelo.ph>
2019-01-24 14:41:43 +0800

    Fix reminder error, if date is not selected

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -81,7 +81,7 @@ class RemindersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def reminder_params
-      params[:reminder][:reminder_date] = Date::strptime(params[:reminder][:reminder_date], "%m/%d/%Y")
+      params[:reminder][:reminder_date].present? ? Date::strptime(params[:reminder][:reminder_date], "%m/%d/%Y") : nil
       params[:reminder][:user_id] = current_user.id
       params.require(:reminder).permit(:carrier_id, :shipper_id, :activity_id, :user_id, :reminder_date, :reminder_interval, :recurring)
     end
