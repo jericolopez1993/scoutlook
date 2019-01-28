794903f89d7197daa9188ddd3c8ed3554de72ddc
Kevin Marcelo <kevin@marcelo.ph>
2019-01-28 13:00:33 +0800

    Fix style on email template

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -44,43 +44,51 @@ class MailsController < ApplicationController
 
     def generate_body
       if @previous_controller == "activities"
-        @content = "<p>Hi, <br/><br/> <b>Here are details about an Activity</b></p>" +
-                   "<p>Campaign: #{@activity.campaign_name}<br/>" +
-                   "Open Date: #{@activity.date_stamp}<br/>" +
-                   "Type: #{@activity.activity_type}<br/>" +
-                   "Contact: #{@activity.carrier_contact ? @activity.carrier_contact.full_name : ''}<br/>" +
-                   "Steward: #{@activity.user ? @activity.user.full_name : ''}<br/>" +
-                   "Annual Value: #{@activity.annual_value}<br/>" +
-                   "Loads Per Week: #{@activity.loads_per_week}<br/>" +
-                   "Status: #{@activity.status ? 'Open' : 'Closed'}<br/>" +
-                   "Date Opened: #{convert_date(@activity.date_opened)}<br/>" +
-                   "Date Closed: #{convert_date(@activity.date_closed)}<br/>" +
-                   "Other Notes: #{@activity.other_notes}<br/>" +
+        @content = "<div style='font-family: Poppins, sans-serif; border: 10px solid #ee9c10; width: 500px; background-color: #fff;'>" +
+                   "<p style='font-size: 50px; background-color: #ee9c10; color: #fff; padding: 1rem; text-align: center; margin-top: 0px;'><b>Here are the details about an Activity</b></p>" +
+                   "<p style='background-color:#fff; margin: 1rem; font-size: 20px; color: #707478'><b style='color: #555; padding-right: 20px;'>Campaign:</b> #{@activity.campaign_name}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Open Date:</b> #{@activity.date_stamp}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Type:</b> #{@activity.activity_type}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Contact:</b> #{@activity.carrier_contact ? @activity.carrier_contact.full_name : ''}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Steward:</b> #{@activity.user ? @activity.user.full_name : ''}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Annual Value:</b> #{@activity.annual_value}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Loads Per Week:</b> #{@activity.loads_per_week}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Status:</b> #{@activity.status ? 'Open' : 'Closed'}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Date Opened:</b> #{convert_date(@activity.date_opened)}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Date Closed:</b> #{convert_date(@activity.date_closed)}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Other Notes:</b> #{@activity.other_notes}<br/>" +
                    "</p>" +
-                   "<br/><br/><p>Thanks, <br/>Scout Logistics</p>"
+                   "<br/><br/><p style='background-color:#fff; margin: 1rem; font-size: 20px; color: #555'>Thanks," +
+                   "<br><img style='width: 50px; vertical-align: middle;' src='https://scouteye.marcelo.ph/assets/scout_fav-5a780324cf2f17ab213cf7ccbbdaa0cca037a75a40500aded47ae7eb33dda6f6.png'>" +
+                   "<b style='vertical-align: middle;'>Scout Logistics</b></p>" +
+                   "</div>"
       elsif @previous_controller == "rates"
-        @content = "<p>Hi, <br/><br/> <b>Here are details about an Rate</b></p>" +
-                   "<p>Origin: #{@rate.origin_city + ", " + @rate.origin_state}<br/>" +
-                   "Destination: #{@rate.destination_city + ", " + @rate.destination_state}<br/>" +
-                   "Carrier: #{@rate.carrier ? @rate.carrier.company_name : ''}<br/>" +
-                   "Shipper: #{@rate.shipper ? @rate.shipper.company_name : ''}<br/>" +
-                   "Contact: #{@rate.carrier_contact ? @rate.carrier_contact.full_name : (@rate.shipper_contact ? @rate.shipper_contact.full_name : '')}<br/>" +
-                   "Activity: #{@rate.activity ? @rate.activity.campaign_name : ''}<br/>" +
-                   "Type: #{@rate.rate_type}<br/>" +
-                   "Picks: #{@rate.picks}<br/>" +
-                   "Drops: #{@rate.drops}<br/>" +
-                   "Team: #{@rate.team}<br/>" +
-                   "Commodities: #{@rate.commodities}<br/>" +
-                   "Bid: #{@rate.bid}<br/>" +
-                   "Ask: #{@rate.ask}<br/>" +
-                   "Accepted: #{@rate.accepted ? 'Yes' : 'No'}<br/>" +
-                   "Cost: #{@rate.cost}<br/>" +
-                   "Currency: #{@rate.money_currency}<br/>" +
-                   "MC Number: #{@rate.mc_number}<br/>" +
-                   "Notes: #{@rate.notes}<br/>" +
-                   "Date Created: #{@rate.created_at}<br/>" +
+        @content = "<div style='font-family: Poppins, sans-serif; border: 10px solid #014b0a; width: 500px; background-color: #fff;'>" +
+                   "<p style='font-size: 50px; background-color: #014b0a; color: #fff; padding: 1rem; text-align: center; margin-top: 0px;'><b>Here are the details about a Rate</b></p>" +
+                   "<p style='background-color:#fff; margin: 1rem; font-size: 20px; color: #707478'><b style='color: #555; padding-right: 20px;'>Origin:</b> #{@rate.origin_city + ", " + @rate.origin_state}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Destination:</b> #{@rate.destination_city + ", " + @rate.destination_state}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Carrier:</b> #{@rate.carrier ? @rate.carrier.company_name : ''}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Shipper:</b> #{@rate.shipper ? @rate.shipper.company_name : ''}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Contact:</b> #{@rate.carrier_contact ? @rate.carrier_contact.full_name : (@rate.shipper_contact ? @rate.shipper_contact.full_name : '')}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Activity:</b> #{@rate.activity ? @rate.activity.campaign_name : ''}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Type:</b> #{@rate.rate_type}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Picks:</b> #{@rate.picks}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Drops:</b> #{@rate.drops}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Team:</b> #{@rate.team}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Commodities:</b> #{@rate.commodities}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Bid:</b> #{@rate.bid}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Ask:</b> #{@rate.ask}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Accepted:</b> #{@rate.accepted ? 'Yes' : 'No'}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Cost:</b> #{@rate.cost}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Currency:</b> #{@rate.money_currency}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>MC Number:</b> #{@rate.mc_number}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Notes:</b> #{@rate.notes}<br/>" +
+                   "<b style='color: #555; padding-right: 20px;'>Date Created:</b> #{@rate.created_at}<br/>" +
                    "</p>" +
-                   "<br/><br/><p>Thanks, <br/>Scout Logistics</p>"
+                   "<br/><br/><p style='background-color:#fff; margin: 1rem; font-size: 20px; color: #555'>Thanks," +
+                   "<br><img style='width: 50px; vertical-align: middle;' src='https://scouteye.marcelo.ph/assets/scout_fav-5a780324cf2f17ab213cf7ccbbdaa0cca037a75a40500aded47ae7eb33dda6f6.png'>" +
+                   "<b style='vertical-align: middle;'>Scout Logistics</b></p>" +
+                   "</div>"
       end
     end
     def generate_contacts
