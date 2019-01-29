8331eb4a0e3b889a143b812b8006fbf67c370bca
Kevin Marcelo <kevin@marcelo.ph>
2019-01-29 18:42:38 +0800

    Fix reminder interval date

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -7,4 +7,18 @@ class Reminder < ApplicationRecord
   def display_name
       "<a href='/reminders/#{self.id}'>Reminder</a>"
   end
+
+  def reminder_date_interval
+    date = self.created_at + self.reminder_interval.days
+    date.strftime("%m/%d/%Y")
+  end
+
+  def reminder_date_reccuring
+    current = self.created_at + self.reminder_interval.days
+    until current >= Date.today
+      current += self.reminder_interval.days
+    end
+    current.strftime("%m/%d/%Y")
+  end
+
 end
