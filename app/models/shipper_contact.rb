de9ca1a7a2850f6a099bcd1209f21b58d1d72737
Kevin Marcelo <kevin@marcelo.ph>
2019-02-14 10:51:27 +0800

    Add TEXT extension if eligible for texting in carrier/shipper table

--- app/models/shipper_contact.rb
+++ app/models/shipper_contact.rb
@@ -2,7 +2,7 @@ class ShipperContact < ApplicationRecord
   audited
   belongs_to :shipper_location, foreign_key: 'location_id', optional: true
   belongs_to :shipper, optional: true
-  has_one :user, primary_key: "id", foreign_key: 'carrier_contact_id'
+  has_one :user, primary_key: "id", foreign_key: 'shipper_contact_id'
 
   def display_name
     if self.shipper.nil?
