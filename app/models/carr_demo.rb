7ccf7e935c46975b0945b354d5f983362b82a06f
Kevin Marcelo <kevin@marcelo.ph>
2019-06-22 22:59:42 +0800

    SC-W25b: Change columns for other carr pages

--- app/models/carr_demo.rb
+++ app/models/carr_demo.rb
@@ -1,7 +1,21 @@
 class CarrDemo < ApplicationRecord
   self.table_name = "carr_demo"
 
-  scope :listings, -> {select('carr_demo.*, (SELECT id FROM carriers WHERE mc_number = carr_demo."MC#" LIMIT 1) AS carrier_id')}
+  scope :listings, -> {select("carr_demo.*,
+    carriers.id AS carrier_id,
+    carriers.sales_priority,
+    CONCAT(relationship_owner_user.first_name, ' ', relationship_owner_user.last_name) AS relationship_owner_name,
+    carriers.relationship_owner AS user_id,
+    CONCAT(contacts.first_name, ' ', contacts.last_name) AS poc_name,
+    contacts.primary_phone,
+    contacts.primary_phone_type,
+    contacts.primary_extension_number,
+    contacts.primary_eligible_texting,
+    contacts.secondary_phone,
+    contacts.secondary_phone_type,
+    contacts.secondary_extension_number,
+    contacts.secondary_eligible_texting"
+  ).joins('LEFT JOIN carriers ON carriers.mc_number = carr_demo."MC#"').joins("LEFT JOIN carrier_contacts AS contacts ON contacts.id = carriers.poc_id").joins("LEFT JOIN users AS relationship_owner_user ON relationship_owner_user.id = carriers.relationship_owner")}
 
   default_scope {listings}
 end
