dc8a07d89d3bb31fd8154079952ecd0e939d37fd
Kevin Marcelo <kevin@marcelo.ph>
2019-11-21 12:07:19 +0800

    Resque Settings Adjustments

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
+rails_env = Rails.env || 'development'
 config_file = rails_root + '/config/resque.yml'
 
 resque_config = YAML::load(ERB.new(IO.read(config_file)).result)
