fc42b16e57ab29fffe2d3f2fb877208a3806bd08
Kevin Marcelo <kevin@marcelo.ph>
2019-11-22 15:17:47 +0800

    Disabled sending email

--- app/jobs/send_compose_mail_job.rb
+++ app/jobs/send_compose_mail_job.rb
@@ -4,7 +4,7 @@ class SendComposeMailJob < ApplicationJob
   def perform(contact, cc, bcc, subject, content, from, attachment_files=nil)
     contact.split(',').map(&:to_s).each do |contact|
       if !contact.strip.empty?
-        MailMailer.send_mail(contact, cc, bcc, subject, content, from, attachment_files).deliver
+        # MailMailer.send_mail(contact, cc, bcc, subject, content, from, attachment_files).deliver
       end
     end
   end
