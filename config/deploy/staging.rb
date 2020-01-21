ae1b21b24593ed57e8456841187448f90e06835a
Kevin Marcelo <kevin@marcelo.ph>
2020-01-21 09:01:40 +0800

    Use SC_Carrier_Ajax on staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :delayed_job_workers, 2
-set :branch, ENV.fetch('REVISION', 'master')
+set :branch, ENV.fetch('REVISION', 'SC_Carrier_Ajax')
 
 
 # Don't change these unless you know what you're doing
