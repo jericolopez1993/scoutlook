5322ee905d54f7e9574821c290892c2a515956b1
Jerico Lopez <jerico.lopez1993@gmail.com>
2020-05-13 19:28:07 +0800

    Rearrange columns and re-adjust column headers and also the carrier name to lessen the column sizes.

--- app/decorators/carr_new_decorator.rb
+++ app/decorators/carr_new_decorator.rb
@@ -6,4 +6,12 @@ class CarrNewDecorator < ApplicationDecorator
     h.render :partial => "carriers/link", :locals => {carrier: object.assigned_carrier}, :formats => [:html, :json]
   end
 
+  def truncate_link_to
+    h.render :partial => "carriers/truncate_link", :locals => {carrier: object.assigned_carrier}, :formats => [:html, :json]
+  end
+
+  def truncate_carrier_name
+    h.render :partial => "carriers/name", :locals => {name: object.carrier_name}, :formats => [:html, :json]
+  end
+
 end
