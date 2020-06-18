2148b3b613de17316e5b14915ecdad0a2b849867
Jerico Lopez <jerico.lopez1993@gmail.com>
2020-06-18 15:54:06 +0800

    Fix the master invoice

--- app/models/master_invoice.rb
+++ app/models/master_invoice.rb
@@ -1,5 +1,5 @@
 class MasterInvoice < ApplicationRecord
-  include Auditable only: [:own_status]
+  include Auditable
   has_one_attached :attachment_file
   def display_name
       if self.invoice_number.present? && self.invoice_number != "" && !self.invoice_number.nil?
