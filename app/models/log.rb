5a404ceebc5f9fdf2d07940388796ba51ca5aeb8
Jerico Lopez <jerico.lopez1993@gmail.com>
2020-06-20 03:30:36 +0800

    Fix the incorrect query.

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,11 +1,11 @@
 class Log < ApplicationRecord
 
   scope :today, -> do
-    select("logs.main_id, MAX(sub_id) AS sub_id, MAX(logs.id) AS id, MAX(model_type) AS model_type, MAX(description) AS description, MAX(logs.created_at) AS created_at, MAX(user_id) AS user_id, MAX(CONCAT(users.first_name, ' ', users.last_name)) as user_name").joins("INNER JOIN users ON users.id = logs.user_id").group("logs.main_id").where.not(:main_id => nil).where.not(:user_id => nil).where("logs.created_at >= ?", Time.zone.today.midnight).order("created_at DESC")
+    select("logs.*, CONCAT(users.first_name, ' ', users.last_name) as user_name").joins("INNER JOIN users ON users.id = logs.user_id").where("(logs.main_id, logs.created_at) IN (SELECT logs2.main_id, MAX(logs2.created_at) FROM logs AS logs2 GROUP BY logs2.main_id)").where.not(:main_id => nil).where.not(:user_id => nil).where("logs.created_at >= ?", Time.zone.today.midnight).order("created_at DESC")
   end
 
   scope :overall, -> do
-    select("logs.main_id, MAX(sub_id) AS sub_id, MAX(logs.id) AS id, MAX(model_type) AS model_type, MAX(description) AS description, MAX(logs.created_at) AS created_at, MAX(user_id) AS user_id, MAX(CONCAT(users.first_name, ' ', users.last_name)) as user_name").joins("INNER JOIN users ON users.id = logs.user_id").group("logs.main_id").where.not(:main_id => nil).where.not(:user_id => nil)
+    select("logs.*, CONCAT(users.first_name, ' ', users.last_name) as user_name").joins("INNER JOIN users ON users.id = logs.user_id").where("(logs.main_id, logs.created_at) IN (SELECT logs2.main_id, MAX(logs2.created_at) FROM logs AS logs2 GROUP BY logs2.main_id)").where.not(:main_id => nil).where.not(:user_id => nil)
   end
 
 end
