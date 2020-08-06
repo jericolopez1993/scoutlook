c8893c1d2be9b738f0de4afeca574576f2d20d31
Jerico Lopez <jerico.lopez1993@gmail.com>
2020-08-06 16:08:30 +0800

    Add Lane Search. Also make sure that it gets the right value from the select2 fields.

--- app/assets/javascripts/carriers.js
+++ app/assets/javascripts/carriers.js
@@ -39,7 +39,6 @@ $(function () {
     "ajax": {
       "url": $('#carrier_table').data('source'),
       "dataSrc": function ( json ) {
-        console.log(json);
         $('#carrier_reefer_count').html(json.reefers_sum);
         $('#carrier_team_count').html(json.teams_sum);
         $('#carrier_lw_count').html(json.loads_lws_sum);
@@ -57,8 +56,6 @@ $(function () {
       }
     },
     "rowCallback": function (row, data) {
-      console.log(data.sales_priority == "<span class='text-indigo'>B</span>");
-      console.log(data.three_weeks_lapse);
       if (data.sales_priority == "<span class='text-indigo'>B</span>" && data.three_weeks_lapse){
         $(row).addClass('table-warning');
       }
@@ -114,6 +111,7 @@ $(function () {
 
   $( "#carrier_table_filter" ).html( "<div style='display: inline-block;'><table><tr><td>Teams:</td><td id='teams_range'></td><td>LW:</td><td id='lws_range'></td></tr><tr><td>Reefers:</td><td id='reefers_range'></td><td>1M:</td><td id='1ms_range'></td></tr><tr><td>DSL:</td><td id='dsls_range'></td><td>6M:</td><td id='6ms_range'></td></tr></table></div>" );
 
+
     table.yadcf([
       {column_number : 1, filter_type: "range_number", filter_reset_button_text: false, filter_container_id: "lws_range"},
       {column_number : 2, filter_type: "range_number", filter_reset_button_text: false, filter_container_id: "1ms_range"},
@@ -123,7 +121,6 @@ $(function () {
       {column_number : 6, filter_type: "text", filter_reset_button_text: false},
       {column_number : 7, data: ['New', 'A', 'B', 'C', 'D', 'U'], filter_default_label: "Prior", filter_reset_button_text: false},
       {column_number : 8, filter_type: "text", filter_reset_button_text: false},
-      {column_number : 9, filter_type: "text", filter_reset_button_text: false},
       {column_number : 10, filter_type: "text", filter_reset_button_text: false},
       {column_number : 11, filter_type: "text", filter_reset_button_text: false},
       {column_number : 12, filter_type: "text", filter_reset_button_text: false},
@@ -142,4 +139,22 @@ $(function () {
     search_term = encodeURI($(this).val());
     table.fnFilter(search_term);
   })
+  $("#from_lanes, #to_lanes").on('keyup change click', function() {
+    var from_lanes_values = $("#from_lanes").val().toString();
+    var to_lanes_values = $("#to_lanes").val().toString();
+    var lane_values = from_lanes_values + "-" + to_lanes_values;
+
+    table.api().columns(9).search(lane_values).draw();
+  });
+  $
+  $("#from_lanes").select2({
+    placeholder: "From",
+    allowClear: true,
+    width: 'resolve',
+    data: originTags });
+  $("#to_lanes").select2({
+    placeholder: "To",
+    allowClear: true,
+    width: 'resolve',
+    data: originTags });
 });
