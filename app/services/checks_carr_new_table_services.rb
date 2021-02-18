5b3439981db33e3b10e75f58bb7f4a838d5929c9
Jerico Lopez <jerico.lopez1993@gmail.com>
2021-02-19 03:12:07 +0800

    Update resque

--- app/services/checks_carr_new_table_services.rb
+++ app/services/checks_carr_new_table_services.rb
@@ -6,8 +6,9 @@ class ChecksCarrNewTableServices
 
   def check_and_move
     sl_carr_news = SlCarrNew.all.pluck(:mc_number)
+    sl_carr_new_names = SlCarrNew.all.pluck(:carrier_name)
     if sl_carr_news.length > 0
-      carr_news = CarrNew.where('mc_number NOT IN (?)', sl_carr_news)
+      carr_news = CarrNew.where('mc_number NOT IN (?) AND carrier_name NOT IN (?)', sl_carr_news, sl_carr_new_names)
     else
       carr_news = CarrNew.all
     end
