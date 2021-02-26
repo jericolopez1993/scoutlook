22527a0ea264ea3bfa29cdeef45b61d7abe6072a
Jerico Lopez <jerico.lopez1993@gmail.com>
2021-02-27 03:55:54 +0800

    Enable kill cron

--- config/deploy.rb
+++ config/deploy.rb
@@ -124,16 +124,16 @@ namespace :deploy do
     end
   end
 
-  # before :starting,     :stop_nginx
+  before :starting,     :stop_nginx
   # before :starting,     :seed_dump
   before :starting,     :check_revision
-  # before :starting,     :kill_cron
+  before :starting,     :kill_cron
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
   after  :finishing,    :stop_cron
-  after  :finishing,    :restart_nginx
-  # after  :finishing,    :start_nginx
+  # after  :finishing,    :restart_nginx
+  after  :finishing,    :start_nginx
   # after  :finishing,    :seed_dump
 end
 
