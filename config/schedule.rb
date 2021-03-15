5dc7d4eea7c88e63c5c6af817f9ee827d457014c
Jerico Lopez <jerico.lopez1993@gmail.com>
2021-03-16 02:47:20 +0800

    Adjust run on every tuesday early morning.

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,13 +22,7 @@ every :day, at: '12:01am', roles: [:worker] do
   rake "reminder:check_due"
 end
 
-every :tuesday, at: '6:00am', roles: [:worker] do
-  rake "computed_data:one_time"
-  rake "computed_data:carr_news"
-  rake "checks_new_carriers:check_and_move"
-end
-
-every :tuesday, at: '2:30pm', roles: [:worker] do
+every :tuesday, at: ['1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '2:30pm'], roles: [:worker] do
   rake "computed_data:one_time"
   rake "computed_data:carr_news"
   rake "checks_new_carriers:check_and_move"
@@ -37,5 +31,4 @@ end
 every :hour, roles: [:worker] do
   rake "computed_data:global_summaries"
   rake "computed_data:next_reminder_date"
-  rake "computed_data:carr_news"
 end
