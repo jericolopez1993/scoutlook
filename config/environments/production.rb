1d21e767410e062a16ab5ca19abddb742929ca16
Jerico Lopez <jerico.lopez1993@gmail.com>
2021-05-10 16:11:09 +0800

    Fix the action cable on production [2]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_cable.allowed_request_origins = ["https://app.scoutlook.ca/", "http://app.scoutlook.ca/"]
+  config.action_cable.allowed_request_origins = ["https://app.scoutlook.ca"]
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
